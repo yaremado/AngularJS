a098cd3b9fdcc30631e4ee7eae8f4e0d8dc2365f
Dima <yaremado@gmail.com>
2016-11-25 18:49:47 +0200

    Almost done

--- app.js
+++ app.js
@@ -4,11 +4,25 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
-$scope.date = "today";
-  $scope.sayHello = function () {
-    return "Hello Coursera!";
+
+  $scope.list_item = "";
+  $scope.message = "";
+
+  $scope.itemCalc = function(){
+    var item_num = 0;
+    if ($scope.list_item != ""){
+   item_num = calcStringItem($scope.list_item)
+    if (item_num > 3) $scope.message = "Too much";
+    if (item_num <= 3) $scope.message = "Good";
+    }
   };
+
+function calcStringItem(string){
+var value = 0;
+var arrays = string.split(',');
+return value = arrays.length;
+};
+
 });
 
 })();
